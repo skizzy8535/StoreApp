8fd21b262486c95de973e6e815967c3953972f29
Yuchen <smirkiiiv@gmail.com>
2022-04-15 21:37:22 +0800

    Initial Commit

---
+++ StoreApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreApp
+//
+//  Created by 林祐辰 on 2022/4/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
